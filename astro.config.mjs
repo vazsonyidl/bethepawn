67580048183d1709155d89fdcf8e216ea97353a4
vazsonyidl <danielvazsonyi@gmail.com>
2023-11-27 18:40:51 +0100

    build: add robots.txt and sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from "@astrojs/sitemap";
 
+const URL = 'https://bethepawn.com';
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  site: URL,
+  integrations: [
+      robotsTxt({
+    sitemap: [
+        `${URL}/sitemap-0.xml`,
+        `${URL}/sitemap-index.xml`,
+    ]
+  }),
+    sitemap()]
+});
