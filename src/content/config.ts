89f47eaa7050edc6ae9b3232e7916d4daa729b05
vazsonyidl <danielvazsonyi@gmail.com>
2024-04-01 16:53:01 +0200

    doc: extend board-game content with all available games

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,9 +5,14 @@ const boardGamesCollection = defineCollection({
     type: 'data', // v2.5.0 and later
     schema: z.object({
         title: z.string(),
-        image: z.string(),
+        image: z.string().optional(),
         description: z.string(),
-        info: z.string(),
+        language: z.string(),
+        'price-per-week': z.string(),
+        deposit: z.string(),
+        players: z.string(),
+        time: z.string(),
+        age: z.string(),
     }),
 });
 
